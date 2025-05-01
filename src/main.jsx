c4b89b72cb0d619d264a8d5dfdef9d6be1cf3d0d
Pratheeksha K N <prathikshakn05@gmail.com>
2025-05-01 23:56:42 +0530

    added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Add this line
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <-- ADD THIS LINE
 import './index.css'; // Your custom global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
